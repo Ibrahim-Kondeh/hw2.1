3ac60ac3bd11a494a342e57333c3799ca89fa313
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-09-17 13:47:55 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -2,17 +2,16 @@
 $pageTitle = "Post result";
 include "view-header.php";
 ?>
+
 <h1>Post result</h1>
- <?php
-if (isset($_POST['my-name'])) {
- ?>
-  <p>The value sent is:</p>
-  <?php
-  echo $_POST['my-name'];
-} else {
-?>
-  <p>Nothing posted to the page.</p>
+
 <?php
+if (isset($_POST['my-name'])) {
+    echo "<p>The value sent is:</p>";
+    echo $_POST['my-name'];
+} else {
+    echo "<p>Nothing posted to the page.</p>";
 }
+
 include "view-footer.php";
 ?>
